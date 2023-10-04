d2311590c622240a4c9071d075e2915754dd8a6c
Martin Dotto <martin_dotto@hotmail.com>
2023-10-04 20:05:21 -0300

    Pre Entrega Fixed JSON e Index

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 
+'use strict'
+
 import express from 'express';
 import http from 'http';
 import path from 'path';
@@ -10,14 +12,14 @@ const httpServer = http.createServer(app);
 // Middlewares
 app.use(express.json());
 
-// Define la ruta al directorio 'public'
+// Ruta al Directorio PUBLIC
 const publicPath = path.resolve(new URL(import.meta.url).pathname, 'public');
 app.use(express.static(publicPath));
 
 // Routes
 app.use('/', router);
 
-// Middleware de errores de ruta
+// Middleware de Errores de Ruta
 const routeError = (req, res) => {
     console.log(req);
     return res.status(404).json({ error: -2, description: `Route '${req.originalUrl}' method '${req.method}' not implemented` });
@@ -30,8 +32,6 @@ const PORT = 8080;
 httpServer.listen(PORT, () => {
     console.log(`Server is running at port ${PORT}`);
 });
-
-'use strict'
 
 console.log('Hello World!')
 
